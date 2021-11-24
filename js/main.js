c5133c636e3d7724443d099d12861fa31e219377
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2021-11-23 19:07:00 -0600

    some background image touchups

--- js/main.js
+++ js/main.js
@@ -14,37 +14,37 @@ racerForm.addEventListener('submit', async (e) =>{
 
     let season = document.createElement('season');
     season.innerHTML = `Season: <br> ${racerData.MRData.RaceTable.season} <br><br> `;
-    season.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    season.style.cssText = 'width:100%; height:100%;';
     document.body.append(season);
 
     let round = document.createElement('round');
     round.innerHTML = `Round: <br> ${racerData.MRData.RaceTable.round}<br><br> `;
-    round.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    round.style.cssText = 'width:100%; height:100%;';
     document.body.append(round); 
 
     let raceName = document.createElement('raceName');
     raceName.innerHTML = `Race Name: <br>  ${racerData.MRData.RaceTable.Races[0].raceName}<br><br> `;
-    raceName.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    raceName.style.cssText = 'width:100%; height:100%;';
     document.body.append(raceName);
 
     let date = document.createElement('date');
     date.innerHTML = `Date: <br> ${racerData.MRData.RaceTable.Races[0].date}<br><br> `;
-    date.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    date.style.cssText = 'width:100%; height:100%;';
     document.body.append(date);
 
     let circuitName = document.createElement('circuitName');
     circuitName.innerHTML = `Circuit Name: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.circuitName}<br><br> `;
-    circuitName.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    circuitName.style.cssText = 'width:100%; height:100%;';
     document.body.append(circuitName);
 
     let locality = document.createElement('locality');
     locality.innerHTML = `City: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.Location.locality}<br><br> `;
-    locality.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    locality.style.cssText = 'width:100%; height:100%;';
     document.body.append(locality);
 
     let country = document.createElement('country');
     country.innerHTML = `Country: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.Location.country}<br><br> `;
-    country.style.cssText = 'width:100%; height:100%; opacity: 0.8; z-index:100;';
+    country.style.cssText = 'width:100%; height:100%;';
     document.body.append(country);
     
 
