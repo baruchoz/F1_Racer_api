225253cfcceda317c21ff83f21f083b698f0d4a4
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2021-11-24 16:00:18 -0600

    final touches

--- js/main.js
+++ js/main.js
@@ -13,37 +13,37 @@ racerForm.addEventListener('submit', async (e) =>{
     let racerData = await getRace(Season, Round);
 
     let season = document.createElement('season');
-    season.innerHTML = `Season: <br> ${racerData.MRData.RaceTable.season} <br><br> `;
+    season.innerHTML = `<div class="container">Season: <br> ${racerData.MRData.RaceTable.season}</div><br><br> `;
     season.style.cssText = 'width:100%; height:100%;';
     document.body.append(season);
 
     let round = document.createElement('round');
-    round.innerHTML = `Round: <br> ${racerData.MRData.RaceTable.round}<br><br> `;
+    round.innerHTML = `<div class="container">Round: <br> ${racerData.MRData.RaceTable.round}</div><br><br> `;
     round.style.cssText = 'width:100%; height:100%;';
     document.body.append(round); 
 
     let raceName = document.createElement('raceName');
-    raceName.innerHTML = `Race Name: <br>  ${racerData.MRData.RaceTable.Races[0].raceName}<br><br> `;
+    raceName.innerHTML = `<div class="container">Race Name: <br>  ${racerData.MRData.RaceTable.Races[0].raceName}</div><br><br> `;
     raceName.style.cssText = 'width:100%; height:100%;';
     document.body.append(raceName);
 
     let date = document.createElement('date');
-    date.innerHTML = `Date: <br> ${racerData.MRData.RaceTable.Races[0].date}<br><br> `;
+    date.innerHTML = `<div class="container">Date: <br> ${racerData.MRData.RaceTable.Races[0].date}</div><br><br> `;
     date.style.cssText = 'width:100%; height:100%;';
     document.body.append(date);
 
     let circuitName = document.createElement('circuitName');
-    circuitName.innerHTML = `Circuit Name: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.circuitName}<br><br> `;
+    circuitName.innerHTML = `<div class="container">Circuit Name: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.circuitName}</div><br><br> `;
     circuitName.style.cssText = 'width:100%; height:100%;';
     document.body.append(circuitName);
 
     let locality = document.createElement('locality');
-    locality.innerHTML = `City: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.Location.locality}<br><br> `;
+    locality.innerHTML = `<div class="container">City: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.Location.locality}</div><br><br> `;
     locality.style.cssText = 'width:100%; height:100%;';
     document.body.append(locality);
 
     let country = document.createElement('country');
-    country.innerHTML = `Country: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.Location.country}<br><br> `;
+    country.innerHTML = `<div class="container">Country: <br> ${racerData.MRData.RaceTable.Races[0].Circuit.Location.country}</div><br><br> `;
     country.style.cssText = 'width:100%; height:100%;';
     document.body.append(country);
     
